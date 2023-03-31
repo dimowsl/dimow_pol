39179b1925a44aeb9fb051cec0ab1ffcaad588d9
dimowsl <127833200+dimowsl@users.noreply.github.com>
2023-03-31 09:04:51 +0300

    Add files via upload

---
+++ dimaka_pol/dimaka_pol/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace dimaka_pol
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.фаилToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.дToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.фToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.аToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.gender = new System.Windows.Forms.GroupBox();
+            this.female = new System.Windows.Forms.RadioButton();
+            this.male = new System.Windows.Forms.RadioButton();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.menuStrip1.SuspendLayout();
+            this.gender.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.фаилToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "Пол";
+            // 
+            // фаилToolStripMenuItem
+            // 
+            this.фаилToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.дToolStripMenuItem,
+            this.фToolStripMenuItem,
+            this.аToolStripMenuItem});
+            this.фаилToolStripMenuItem.Name = "фаилToolStripMenuItem";
+            this.фаилToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.фаилToolStripMenuItem.Text = "фаил";
+            // 
+            // дToolStripMenuItem
+            // 
+            this.дToolStripMenuItem.Name = "дToolStripMenuItem";
+            this.дToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.дToolStripMenuItem.Text = "нов";
+            this.дToolStripMenuItem.Click += new System.EventHandler(this.дToolStripMenuItem_Click);
+            // 
+            // фToolStripMenuItem
+            // 
+            this.фToolStripMenuItem.Name = "фToolStripMenuItem";
+            this.фToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.фToolStripMenuItem.Text = "запази";
+            this.фToolStripMenuItem.Click += new System.EventHandler(this.фToolStripMenuItem_Click);
+            // 
+            // аToolStripMenuItem
+            // 
+            this.аToolStripMenuItem.Name = "аToolStripMenuItem";
+            this.аToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.аToolStripMenuItem.Text = "изход";
+            this.аToolStripMenuItem.Click += new System.EventHandler(this.аToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Название";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Име";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(88, 72);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(225, 72);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // gender
+            // 
+            this.gender.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.gender.Controls.Add(this.male);
+            this.gender.Controls.Add(this.female);
+            this.gender.Location = new System.Drawing.Point(49, 165);
+            this.gender.Name = "gender";
+            this.gender.Size = new System.Drawing.Size(227, 90);
+            this.gender.TabIndex = 5;
+            this.gender.TabStop = false;
+            this.gender.Text = "Пол";
+            // 
+            // female
+            // 
+            this.female.AutoSize = true;
+            this.female.Location = new System.Drawing.Point(22, 53);
+            this.female.Name = "female";
+            this.female.Size = new System.Drawing.Size(54, 17);
+            this.female.TabIndex = 0;
+            this.female.TabStop = true;
+            this.female.Text = "Жена";
+            this.female.UseVisualStyleBackColor = true;
+            this.female.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // male
+            // 
+            this.male.AutoSize = true;
+            this.male.Location = new System.Drawing.Point(22, 30);
+            this.male.Name = "male";
+            this.male.Size = new System.Drawing.Size(49, 17);
+            this.male.TabIndex = 1;
+            this.male.TabStop = true;
+            this.male.Text = "Мъж";
+            this.male.UseVisualStyleBackColor = true;
+            this.male.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(88, 44);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.gender);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = " m";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.gender.ResumeLayout(false);
+            this.gender.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem фаилToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem дToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem фToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem аToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.GroupBox gender;
+        private System.Windows.Forms.RadioButton male;
+        private System.Windows.Forms.RadioButton female;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ dimaka_pol/dimaka_pol/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dimaka_pol
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void дToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void фToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void аToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
